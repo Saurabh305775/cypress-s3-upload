b7edd6fefb0d507193d365eddf8f7a1774badbcf
Saurabh Kumar <mail2saurabh19@gmail.com>
2025-04-19 18:40:23 -0500

    Add GitHub Action for Cypress + S3

--- upload-to-s3.js
+++ upload-to-s3.js
@@ -1,19 +1,23 @@
+require('dotenv').config();
+console.log("Loaded bucket name from .env:", process.env.AWS_BUCKET);
+
+const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
-const AWS = require('aws-sdk');
 const archiver = require('archiver');
 const dayjs = require('dayjs');
 
-const timestamp = dayjs().format('YYYYMMDD-HHmmss');
-const zipFilename = `results_${timestamp}.zip`;
-const screenshotsDir = path.join(__dirname, 'screenshots');
+const screenshotsDir = 'cypress/screenshots';
+const zipFilename = `results_${dayjs().format('YYYYMMDD-HHmmss')}.zip`;
 const zipPath = path.join(__dirname, zipFilename);
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION
 });
+
+const BUCKET_NAME = process.env.AWS_BUCKET;
 
 function zipScreenshots() {
   return new Promise((resolve, reject) => {
@@ -31,20 +35,20 @@ function zipScreenshots() {
 
 function uploadToS3() {
   const fileContent = fs.readFileSync(zipPath);
-
   const params = {
-    Bucket: process.env.AWS_BUCKET,
+    Bucket: BUCKET_NAME,
     Key: zipFilename,
     Body: fileContent,
   };
 
+  console.log("Uploading to bucket:", BUCKET_NAME);
   return s3.upload(params).promise();
 }
 
 (async () => {
   try {
     await zipScreenshots();
-    console.log(`Zipped screenshots as ${zipFilename}`);
+    console.log('Zipped screenshots as ${zipFilename}');
     await uploadToS3();
     console.log(`Uploaded to S3 as ${zipFilename}`);
   } catch (err) {
